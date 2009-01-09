395b0f961541b48ab4b677caccb30ef56fa2bbde
Eric Walkingshaw <eric@walkingshaw.net>
2009-01-09 21:14:47 +0000

    Reordered functions.
    git-svn-id: svn+ssh://access.engr.oregonstate.edu/nfs/guille/u2/w/walkiner/lib/repository/game-theory/trunk/code@231 b70662a2-c83c-0410-ba1d-9e7d568b256d

--- Hagl/Exec/Run.hs
+++ Hagl/Exec/Run.hs
@@ -12,6 +12,14 @@ import Hagl.Exec
 -- Game Execution --
 --------------------
 
+once :: (Game g) => ExecM g ()
+once = do p <- runGame
+          conclude p
+
+times :: (Game g) => Int -> ExecM g ()
+times 0 = return ()
+times n = once >> times (n-1)
+
 conclude :: Game g => Payoff -> ExecM g ()
 conclude p = 
   do e <- getExec
@@ -20,11 +28,3 @@ conclude p =
      put e { _gameState = initState (_game e),
              _transcript = [],
              _history = ByGame ((t,(ms,p)) : toList (_history e)) }
-
-once :: (Game g) => ExecM g ()
-once = do p <- runGame
-          conclude p
-
-times :: (Game g) => Int -> ExecM g ()
-times 0 = return ()
-times n = once >> times (n-1)
