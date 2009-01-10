6ce3d4d20f0d4721a0c6fa0330841bf9d464c895
Eric Walkingshaw <eric@walkingshaw.net>
2009-01-10 00:29:06 +0000

    Removed Util file.
    git-svn-id: svn+ssh://access.engr.oregonstate.edu/nfs/guille/u2/w/walkiner/lib/repository/game-theory/trunk/code@237 b70662a2-c83c-0410-ba1d-9e7d568b256d

--- Hagl/Util.hs
+++
@@ -1,9 +0,0 @@
-module Hagl.Util where
-
-import Control.Monad.State
-import Data.List
-
--- TODO: Organize this file, figure out what's needed where, etc.
-
-update :: MonadState s m => (s -> s) -> m s
-update f = modify f >> get
